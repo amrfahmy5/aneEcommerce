cc93da517bf705f8a05ed3fb1d2c6b9717ec15b5
amrfahmy5 <amrfahmy3210@gmail.com>
2024-03-06 10:04:50 +0200

    style chat button

--- client/app/components/Store/conntact/index.js
+++ client/app/components/Store/conntact/index.js
@@ -9,28 +9,38 @@ const ChatWidget = () => {
 
   const handleWhatsAppButtonClick = () => {
     const whatsappNumber = "+201121833830";
-    window.location.href = `https://wa.me/${whatsappNumber}`;
+    // window.location.href = `https://wa.me/${whatsappNumber}`;
+    window.open(
+      `https://wa.me/${whatsappNumber}`,
+      '_blank' // <- This is what makes it open in a new window.
+    );
   };
 
   const handleCallButtonClick = () => {
     const callNumber = "+201121833830";
     window.location.href = `tel:${callNumber}`;
   };
+  const handleChatButtonClick = () => {
+    window.location.href = `/support`;
+  };
 
   return (
     <div id="chat-widget">
         {showButtons && (
         <>
-          <button id="whatsapp-button"  className="chat-button" onClick={handleWhatsAppButtonClick}>
-          <i class="fa-brands fa-whatsapp"></i>
+        <button className="chat-button another-button" onClick={handleChatButtonClick}>
+            <i className="fa-brands fa-rocketchat"></i>
           </button>
-          <button id="call-button"  className="chat-button" onClick={handleCallButtonClick}>
-            <i className="fa fa-phone"></i>
+          <button className="chat-button another-button" onClick={handleWhatsAppButtonClick}>
+          <i className="fa-brands fa-whatsapp"></i>
+          </button>
+          <button className="chat-button another-button" onClick={handleCallButtonClick}>
+            <i className="fa-solid fa-phone-volume"></i>
           </button>
         </>
       )}
       <button id="main-button" className="chat-button" onClick={handleMainButtonClick}>
-        <i class="fa-solid fa-headset"></i>
+        <i className="fa-solid fa-headset"></i>
       </button>
     </div>
   );
